bb83a3bc6895cd50bfc986f5e9370d2604f8c1d6
Mohamed50 <Mohamedalameen50@gmail.com>
2024-10-25 13:23:13 +0400

    downgrade web version

--- example/windows/flutter/generated_plugin_registrant.cc
+++ example/windows/flutter/generated_plugin_registrant.cc
@@ -7,11 +7,8 @@
 #include "generated_plugin_registrant.h"
 
 #include <customerpulse_flutter/customerpulse_flutter_plugin_c_api.h>
-#include <flutter_inappwebview_windows/flutter_inappwebview_windows_plugin_c_api.h>
 
 void RegisterPlugins(flutter::PluginRegistry* registry) {
   CustomerpulseFlutterPluginCApiRegisterWithRegistrar(
       registry->GetRegistrarForPlugin("CustomerpulseFlutterPluginCApi"));
-  FlutterInappwebviewWindowsPluginCApiRegisterWithRegistrar(
-      registry->GetRegistrarForPlugin("FlutterInappwebviewWindowsPluginCApi"));
 }
